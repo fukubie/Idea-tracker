7c2d9470b82d69b3543af3a8844da8dad1de1636
fukubie <abdonssir321@icloud.com>
2026-03-13 20:44:45 -0400

    chore: update .env.example with Appwrite project and endpoint URLs; enhance appwrite.js to include Functions

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,10 +1,11 @@
-import { Client, Databases, Account, Storage } from "appwrite";
+import { Client, Databases, Account, Storage, Functions } from "appwrite";
 
 const client = new Client();
 client
-  .setEndpoint(`${process.env.REACT_APP_APPWRITE_ENDPOINT}`)
-  .setProject(`${process.env.REACT_APP_APPWRITE_PROJECT_ID}`);
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT || "")
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID || "");
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
+export const functions = new Functions(client);
